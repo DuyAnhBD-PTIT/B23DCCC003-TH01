f6a41238b191e78c800951f9c69fd9f15c4d97bf
DuyAnhBD-PTIT <AnhBDD.B23CC003@stu.ptit.edu.vn>
2024-10-09 15:44:42 +0700

    Routes

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Changed Switch to Routes
 import RandomColor from './RandomColor/RandomColor'; 
 import TodoApp from './TodoList/index';
-import './App.css'
+import './App.css';
 import ImageFind from './ImageList/ImageFind';
 
 function App() {
   return (
-    <div className='container'>
-        <div className='TodoApp'><TodoApp /></div>
-        <div className='RandomColor'><RandomColor /></div>
-        <div className='ImageFind'><ImageFind /></div>
-    </div>
+    <Router>
+      <div className='container'>
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li><Link to="/">Todo App</Link></li>
+            <li><Link to="/random-color">Random Color</Link></li>
+            <li><Link to="/image-find">Image Find</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<TodoApp />} />
+          <Route path="/random-color" element={<RandomColor />} />
+          <Route path="/image-find" element={<ImageFind />} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
-export default App
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from 'react-dom/client'; // Ensure you are using the correct import
+import './index.css'; // Make sure this file exists and is loaded correctly
+import App from './App'; // Ensure App.js exports a valid component
+import reportWebVitals from './reportWebVitals'; // Optional performance tracking
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Getting the root element
 root.render(
   <React.StrictMode>
-    <App/>
+    <App /> {/* Rendering the App component */}
   </React.StrictMode>
 );
-reportWebVitals();
+
+reportWebVitals(); // Optional: for measuring performance
